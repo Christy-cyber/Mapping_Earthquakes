0a7de14ccf9b31953f7bfcac05b9aeae35f42837
unknown <drctcarter@gmail.com>
2022-03-13 15:26:55 -0400

    Updaing logic.js file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,11 +2,11 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map using Leaflet documentation.
+//    Create the tile layer that will be the background of our map using Leaflet documentation.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
- //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //    maxZoom: 18,
 //    id: 'mapbox/streets-v11',
 //    tileSize: 512,
@@ -14,9 +14,19 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //    accessToken: API_KEY
 //});
 
-// Code for doing the above using Mapbox Syles API
-// We create the tile layer that will be the background of our map.
- let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Add a circleMarker to the map for Los Angeles, California, with a 300 pixel radius.
+let marker = L.circleMarker([34.0522, -118.2437], {
+      radius: 300,
+      color: "black",
+      fillColor: '#ffffa1'
+}).addTo(map);
+// To add a marker for LA, use: L.marker([34.0522, -118.2437]).addTo(map);
+// To add a circle with a 100 m radius, use: L.circle([34.0522, -118.2437], {
+//    radius: 100
+
+// Code for doing the above using Mapbox Syles API below
+// Create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
  maxZoom: 18,
  accessToken: API_KEY
